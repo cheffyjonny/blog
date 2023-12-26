cd817579a90d6414a2ed40dd3fa5def376159ba6
John Lee <john.lee24h@gmail.com>
2023-12-26 22:43:37 +0900

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,89 @@
+const routes = [
+  "Vue",
+  "React",
+  "TS",
+  "etc",
+  "Playground"
+];
+
+const createSidebar = () => {
+  const sidebar = {};
+  for (const route of routes) {
+    Object.assign(sidebar, require("../" + route));
+  }
+  return sidebar;
+};
+
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Step By Step',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: 'Personal notepad for understanding than memorizing',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'React',
+        link: '/React/',
+      },
+      {
+        text: 'TS',
+        link: '/TS/'
+      },
+      {
+        text: 'Vue',
+        link: '/Vue/'
+      },
+      {
+        text: 'etc',
+        link: '/etc/'
+      },
+      {
+        text: 'Playground',
+        link: '/Playground/'
+      },
+      {
+        text: 'Info',
+        items: [
+          { text: 'Github', link: 'https://v1.vuepress.vuejs.org' },
+          { text: 'Contact', link: '/Playground/'  }
+        ]
+      }
+    ],
+    sidebar: createSidebar(),
+  },
+  base: "/blog/",
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+# heroImage: https://v1.vuepress.vuejs.org/hero.png
+
+actionText: View All
+actionLink: /React/
+features:
+- title: React
+  details: React V18 examples and issues
+- title: Playground
+  details: Quiz and forked examples
+- title: Diary
+  details: Experienced issues and daily study
+footer: Made by John Lee with ♡
+---
